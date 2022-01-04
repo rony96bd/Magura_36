a6774b6c046eeca6096615662c15ea6e53b073eb
Md. Rakib Uddin <r.rony9966@gmail.com>
2022-01-04 15:14:53 +0600

    day2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -15,40 +15,49 @@ class MyApps extends StatelessWidget {
           title: Text("Login Page"),
         ),
         body: Material(
-          child: Column(
-            children: [
-              SizedBox(
-                  height: 200,
-                  width: 500,
-                  child: Image.asset(
-                    "assets/images/login.png",
-                    fit: BoxFit.contain,
-                  )),
-              Text(
-                "Please Login",
-                style: TextStyle(fontSize: 30, color: Colors.blueAccent),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0, right: 15),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Enter your Username",
-                        labelText: "User ID",
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Enter your Password",
-                        labelText: "Enter Password",
-                      ),
-                    ),
-                    ElevatedButton(onPressed: () {}, child: Text("Login"))
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                    height: 200,
+                    width: 500,
+                    child: Image.asset(
+                      "assets/images/login.png",
+                      fit: BoxFit.contain,
+                    )),
+                Text(
+                  "Please Login",
+                  style: TextStyle(fontSize: 30, color: Colors.blueAccent),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0, right: 15),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter your Username",
+                          labelText: "User ID",
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter your Password",
+                          labelText: "Enter Password",
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Login"),
+                        style: TextButton.styleFrom(minimumSize: Size(100, 45)),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
