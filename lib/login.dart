bdc7a449f4da6f28cc5fe7f8f5807738ce353365
Md. Rakib Uddin <r.rony9966@gmail.com>
2022-01-09 00:04:44 -0700

    day2 complete

---
+++ lib/login.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Material(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                  height: 200,
+                  width: 500,
+                  child: Image.asset(
+                    "assets/images/login.png",
+                    fit: BoxFit.contain,
+                  )),
+              Text(
+                "Please Login",
+                style: TextStyle(fontSize: 30, color: Colors.blueAccent),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0, right: 15),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter your Username",
+                          labelText: "User ID",
+                          prefixIcon: Icon(Icons.account_circle)),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter your Password",
+                          labelText: "Enter Password",
+                          prefixIcon: Icon(Icons.password)),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: Size(130, 45)),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/login.dart';
 
 void main() {
   runApp(MyApps());
@@ -10,57 +11,9 @@ class MyApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Login Page"),
-        ),
-        body: Material(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                    height: 200,
-                    width: 500,
-                    child: Image.asset(
-                      "assets/images/login.png",
-                      fit: BoxFit.contain,
-                    )),
-                Text(
-                  "Please Login",
-                  style: TextStyle(fontSize: 30, color: Colors.blueAccent),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0, right: 15),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Enter your Username",
-                          labelText: "User ID",
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Enter your Password",
-                          labelText: "Enter Password",
-                        ),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Login"),
-                        style: TextButton.styleFrom(minimumSize: Size(200, 45)),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+      routes: {
+        "/": (context) => LoginPage(),
+      },
     );
   }
 }
